6347ecdbabb7fbd9b1af1411ff32bcbbe19c902f
Diana <freed@li.ru>
2021-07-08 19:59:08 +0000

    new data and list of nodes

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
+import json
 import streamlit as st
 from streamlit_agraph import agraph, Node, Edge, Config
 
 st.title("Graph Example")
+st.title("Graph Example")
 nodes = []
 edges = []
-data = [('Джеймс Мэй', 'Джереми Кларксон'),
- ('Ричард Хаммонд', 'Джереми Кларксон'),
- ('Антон Савенков', 'Джереми Кларксон', 'Топ Гир'),
- ('Ричард Хаммонд', 'Джеймс Мэй'),
- ('Антон Савенков', 'Джеймс Мэй', 'Топ Гир'),
- ('Антон Савенков', 'Ричард Хаммонд', 'Топ Гир')]
-
-for row in data:
-    nodes.append(Node(id=row[0], label=row[0], size=400))
-    nodes.append(Node(id=row[1], size=400) )
-    edges.append(Edge(source=row[0], target=row[1]))
-config = Config(width=1000, 
+with open("list.txt", "r") as f:
+    data = json.loads(f.read())
+    print(data)
+    for row in data:
+        nodes.append(Node(id=row[0], label=row[0], size=400))
+        nodes.append(Node(id=row[1], size=400) )
+        edges.append(Edge(source=row[0], target=row[1]))
+    config = Config(width=1000, 
                 height=1000, 
                 directed=False,
                 nodeHighlightBehavior=True, 
@@ -26,6 +24,5 @@ config = Config(width=1000,
                 # **kwargs e.g. node_size=1000 or node_color="blue"
                 ) 
 
-return_value = agraph(nodes=nodes, 
-                      edges=edges, 
-                      config=config)
+    return_value = agraph(nodes=nodes, 
+                      edges=edges, config=config)
--- parser.py
+++ parser.py
@@ -1,30 +1,29 @@
 import requests
 import csv
 
-#token = "612eeca9a1ea9612e2e309b86f753f78"
-token = "aa32b492b017cb38c892c806ace7601d"
+token = "612eeca9a1ea9612e2e309b86f753f78"
+#token = "aa32b492b017cb38c892c806ace7601d"
 item = "tv-series"
+   
+with open("srl.txt", 'r') as l:
+    data = []
+    for link in l.readlines():
+        item_id = link.strip()
+        link = f"https://api.kinopoisk.cloud/{item}/{item_id}/token/{token}"
 
-with open("data.csv", 'w', newline='') as f:
-    fields = ['title', 'actor']
+        result = requests.get(link)
+        
+        if result.status_code == 200:
+            res = result.json()
+            for actor in res["actors"]: 
+                data.append({"actor": actor, "title": res["title"]})                   
+        else:
+            print("Houston, we have a problem!", result.status_code)
+
+
+with open("data3.csv", 'w', newline='') as f:
+    fields = ['actor', 'title']
     writer = csv.DictWriter(f, fields, delimiter=';')
     writer.writeheader()
-    data = []
-    
-    with open("srl.txt", 'r') as l:
-      
-        for link in l.readlines():
-            item_id = link.strip()
-            link = f"https://api.kinopoisk.cloud/{item}/{item_id}/token/{token}"
-
-            result = requests.get(link)
-            
-            if result.status_code == 200:
-                res = result.json()
-                for actor in res["actors"]: 
-                    data.append({"title": res["title"], "actor": actor})                   
-            else:
-                print("Houston, we have a problem!", result.status_code)
-
-        for row in data:
-            writer.writerow(row)            
+    for row in data:
+        writer.writerow(row)            
